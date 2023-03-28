fbe7a1c7135c183109516aef48543cacbc3ddd75
vivit_jc <kazyu.vanilla@gmail.com>
2023-03-28 23:57:54 +0900

    youtubeとtwitterがリンク置換されてしまうのを修正

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,7 +1,11 @@
 export function modText(text: string){
   // 今のところリンクは一つだけ対応
   const link = text.match(/http.+/)
-  if(link){
+  if(new RegExp('https:\/\/twitter.com\/.+\/status\/\[0-9]+').test(text)){
+
+  } else if(new RegExp(/(youtube\.com|youtu\.be)/g).test(text)){
+
+  } else if(link){
     text = text.replace(link[0],`<a target=\"_\" href=\"${link[0]}\">${link[0]}<\/a>`)
   }
   return text
